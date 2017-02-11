bf14ec024ffd8cebf0717503e2b6dc5b980cb318
Alexander Curtiss <apccurtiss@gmail.com>
2017-02-11 10:12:04 -0700

    Movedmatchmaking init into function

--- server.js
+++ server.js
@@ -3,29 +3,38 @@ var appName = 'Vegan Tornadoes';
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 8083;
-var path = require('path');
 var Matchmaker = require('matchmaker');
+var path = require('path');
+var port = process.env.PORT || 8083;
 
 // stuff that came in handy before...
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-matchQueue = new Matchmaker;
 
-matchQueue.policy = function(a,b) {
-    if(Maths.abs(a.rank-b.rank) < 20)
-        return 100
-    else
-        return 0
+function queueInit() {
+  var mm = new Matchmaker;
+
+  mm.prefs.checkinterval = 5000;
+  mm.prefs.threshold = 100;
+  mm.prefs.maxiters = 50;
+
+  mm.policy = function(a,b) {
+      if(Maths.abs(a.rank-b.rank) < 20)
+          return 100
+      else
+          return 0
+  }
+
+  //@TODO(alex): Check to see if users have left disconnected
+  mm.on('match', function(result) {
+      console.log(result.a + " has been matched with " + result.b)
+  });
+
+  mm.start();
+  return mm;
 }
 
-//@TODO : Check to see if users have left disconnected
-matchQueue.on('match', function(result) {
-    console.log(result.a + " has been matched with " + result.b)
-});
-
-matchQueue.start();
-
+matchQueue = queueInit();
 /*
  * How to add people to queue:
  * matchQueue.push({name: 'walter',rank:1450})
